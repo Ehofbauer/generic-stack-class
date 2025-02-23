ab719e42017dd633be9d890b75285fa7d3832f0f
Ehofbauer <ehofbauer@student.tgm.ac.at>
2025-02-23 22:07:29 +0100

    Test class finished

---
+++ src/src/StackTest.java
@@ -0,0 +1,25 @@
+/**
+ * Test class for Stack.
+ */
+public class StackTest {
+    public static void main(String[] args) {
+        try {
+            Stack<Integer> numberStack = new Stack<>();
+            numberStack.push(10);
+            numberStack.push(20);
+            numberStack.push(30);
+            System.out.println("Number Stack (list): " + numberStack.list());
+            System.out.println("Popped: " + numberStack.pop());
+            System.out.println("Peek: " + numberStack.peek());
+
+            Stack<String> textStack = new Stack<>();
+            textStack.push("Hello");
+            textStack.push("World");
+            System.out.println("Text Stack (list): " + textStack.list());
+            System.out.println("Popped: " + textStack.pop());
+
+        } catch (StackFullException | StackEmptyException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
